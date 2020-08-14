f2142efeda91caddea09489d5d5ce5648dd0ccae
Erik Rasmus Nilsson <info@rasmusnilsson.se>
2020-08-14 11:45:55 +0200

    Addded todos and updated render

--- batty.c
+++ batty.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <pthread.h>
 
+// TODO: read path from conf file
 #define PATH "/sys/class/power_supply/BAT0/capacity"
 #define CAPACITY 100
 
@@ -16,26 +17,27 @@ void *update_battery_status(void *arg)
 	if ((file = fopen(PATH, "r")) == NULL)
 	{
 		printf("Error opening file: %s\n", PATH);
+		return;
 	}
 	fscanf(file, "%d", battery);
-	if (DEBUG == 1) printf("\rCurrent battery status: %d\n", *battery);
 	fclose(file);
-	sleep(2);
+	sleep(10); // read from config file
 }
 
 void print_battery(int battery) 
 {
-	int battery_norm = 1;
-	int section_size = CAPACITY / 3;
-	printf("\r\033[2A");
-	for (int j = 0; j < 2; j++)
+	int battery_norm = 2; // TODO: add to conf file
+	int battery_thickness = 5; // TODO: add to conf file
+	int section_size = CAPACITY / battery_norm  / 4;
+	printf("\r\033[%dA", battery_thickness);
+	for (int j = 0; j < battery_thickness; j++)
 	{
 		for (int i = 0; i < battery / battery_norm; i++) 
 		{
 			if (i < section_size) printf("\033[0;31m");
 			else if (i < section_size * 2) printf("\033[0;33m");
 			else printf("\033[0;32m");
-			printf("[");
+			printf("\u2588");
 		}
 		printf("\033[0;34m");
 		for (int i = 0; i < CAPACITY / battery_norm - battery / battery_norm; i++) 
@@ -45,6 +47,7 @@ void print_battery(int battery)
 		printf("\033[0;0m");
 		printf("\n");
 	}
+	printf("Current charge: %d%%", battery);
 	fflush(stdout);
 }
 
@@ -62,7 +65,7 @@ void print_header()
 	while (fgets(buffer, 128, file) != NULL) printf("%s", buffer);
 	fclose(file);
 	printf("\033[0;0m");
-	printf("\n\n");
+	printf("\n\n\n\n\n\n");
 }
 
 
